77caf95c05e3b58cd38c88d4ec96e90cbc0ffbbd
Guilherme Dinis Jr <guilherme.dinisjunior@gmail.com>
2016-05-01 14:51:33 +0200

    fixed dbcontext config annotation

--- src/main/java/org/dsc/diseametry/DbContext.java
+++ src/main/java/org/dsc/diseametry/DbContext.java
@@ -3,11 +3,12 @@ package org.dsc.diseametry;
 import org.dsc.diseametry.repositories.DiseaseRepository;
 import org.dsc.diseametry.repositories.IndicatiorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
-import org.springframework.test.context.ContextConfiguration;
 
 @Service
-@ContextConfiguration("file:src/main/resources/spring/context.xml")
+//@ContextConfiguration("file:src/main/resources/spring/context.xml")
+@Configuration("file:src/main/resources/spring/context.xml")
 public class DbContext {
 
 	@Autowired
